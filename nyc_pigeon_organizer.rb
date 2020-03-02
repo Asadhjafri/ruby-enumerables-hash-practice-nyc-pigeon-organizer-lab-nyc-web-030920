7d9b04002de859603340fb05cdd5d5e3884ced38
Asad Jafri <asadhjafri@gmail.com>
2020-03-02 19:24:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,45 +1,22 @@
+
 def nyc_pigeon_organizer(data)
-  pigeons = {
-    "Theo" => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Subway"]
-    },
+new_hash = {}
+data.each do |key, value|
+  value.each do |new_value, names|
+    names.each do |name|
 
-    "Peter Jr." => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Library"]
-    },
+      if !new_hash[name]
+        new_hash[name] = {}
+      end
 
-    "Lucky" => {
-      :color => ["purple"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    },
+      if !new_hash[name][key]
+        new_hash[name][key] = []
+      end
 
-    "Ms. K" => {
-      :color => ["grey", "white"],
-      :gender => ["female"],
-      :lives => ["Central Park"]
-    },
+      new_hash[name][key] << new_value.to_s
 
-    "Queenie" => {
-      :color => ["white", "brown"],
-      :gender => ["female"],
-      :lives => ["Subway"]
-    },
-
-    "Andrew" => {
-      :color => ["white"],
-      :gender => ["male"],
-      :lives => ["City Hall"]
-    },
-
-    "Alex" => {
-      :color => ["white", "brown"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    }
-  }
+    end
+  end
+end
+new_hash
 end
