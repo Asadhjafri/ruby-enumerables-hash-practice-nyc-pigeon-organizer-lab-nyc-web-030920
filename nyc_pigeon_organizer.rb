5952dcda99e73be91f7cd24d87a39b76d3d14eaa
Asad Jafri <asadhjafri@gmail.com>
2020-03-02 19:21:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -44,19 +44,4 @@ def nyc_pigeon_organizer(data)
   }
 end
 
-def nyc_pigeon_organizer(data)
-  pigeon_data = {
-  :color => {
-    :brown => ["Luca"],
-    :black => ["Lola"],
-  },
-  :gender => {
-    :male => ["Luca"],
-    :female => ["Lola"]
-  },
-  :lives => {
-    "Central Park" => ["Lola"],
-    "Library" => ["Luca"]
-  }
-}
-end
+
